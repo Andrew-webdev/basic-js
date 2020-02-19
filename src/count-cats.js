eaa1f20de46dcbc1a9b354ca2077ab3a1c182a5a
Andrew-webdev <andreyrow96@gmail.com>
2020-02-19 17:40:42 +0300

    feat: count - cats is solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let unitArr = [].concat(...matrix);
+  let count = 0;
+  if (unitArr.length > 0) {
+    for (let i = 0; i < unitArr.length; i++) {
+      if (unitArr[i] === "^^") {
+        count++;
+      }
+    }
+    return count;
+  } else {
+    return 0;
+  }
+}
